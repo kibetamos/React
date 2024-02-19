0ddc57907adc73247a977e682cee97eec8e624e0
kibetamos <amos.rotich@strathmore.edu>
2024-02-19 11:31:15 +0300

    Imperative way to write code

--- FirstReact/index.js
+++ FirstReact/index.js
@@ -6,8 +6,24 @@ the line of code you just learned! Nope, not on my watch!
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 
-ReactDOM.render(
+// ReactDOM.render(
 
-<ul><li>we are </li> </ul>, 
+// <ul><li>we are </li> </ul>, 
 
-document.getElementById('root'))
+// document.getElementById('root'))
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
