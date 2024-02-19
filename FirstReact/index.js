bd13dbdfb80d2aa31e40f941fe899c34652d675d
kibetamos <amos.rotich@strathmore.edu>
2024-02-19 11:07:11 +0300

    added react libraries

--- FirstReact/index.js
+++ FirstReact/index.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+// ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+ReactDOM.render(
+
+<ul><li>we are </li> </ul>, 
+
+document.getElementById('root'))
