d0c26719bf3c2037c2cc7b83faf6c06d0ad9e20b
kibetamos <amos.rotich@strathmore.edu>
2024-02-19 15:49:47 +0300

    Added Custompage component - PART 2

--- Project1/index.js
+++ Project1/index.js
@@ -75,10 +75,20 @@ function Page(){
     )
 }
 
-// ReactDOM.render(
-//     <div>
-//         <Page />
-//     </div>,
-//     document.getElementById("root")
-// )
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+
+ */
+
+
 ReactDOM.render(<Page />, document.getElementById("root"))
