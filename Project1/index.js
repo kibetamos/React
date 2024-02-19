9cff89df228320dadcc072a13e990bf9a5c622d9
kibetamos <amos.rotich@strathmore.edu>
2024-02-19 16:48:26 +0300

    Added Custompage component -

--- Project1/index.js
+++ Project1/index.js
@@ -62,11 +62,29 @@ Mini Challenge:
 Move the `header` element from Page into 
 its own component called "Header"
 */
+
+
+/**
+Challenge: 
+
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+- Using flexbox, line up the nav items horizontally, and
+  put them inline with the React logo.
+
+- Change the image styling to happen in CSS instead of in-line
+  For practice, add a new class to the image in order to style it
+*/
 function Header() {
     return (
         <header>
-            <nav>
+            <nav className="nav">
                 <img src="./react-logo.png" width="40px" />
+                <ul className= "nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
@@ -75,11 +93,9 @@ function Header() {
 function Page(){
     return(
         <div>
-           <Header />
+        <Header />
         <MainComponent />
-            {/* <p>This is my page</p> */}
-
-            <Footer />
+        <Footer />
         </div>
     )
 }
