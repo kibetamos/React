6473a0cb7e51aa49a906099912844aec2b5214a7
kibetamos <amos.rotich@strathmore.edu>
2024-02-19 12:19:15 +0300

    Adding nav bar

--- FirstReact/index.js
+++ FirstReact/index.js
@@ -60,21 +60,45 @@ appending an h1 to our div#root (without using innerHTML).
 //     - Don't worry about styling yet - it'll just be plain-looking HTML for now
 // */
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+// const navbar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
 
+// )
+
+// console.log(navbar)
+
+// ReactDOM.render(
+//     navbar, 
+//     document.getElementById('root')
+// )
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
 )
 
-console.log(navbar)
+// ReactDOM.render(page, document.getElementById("root"))
+// document.getElementById("root").append(JSON.stringify(page))
 
 ReactDOM.render(
-    navbar, 
-    document.getElementById('root')
+    page,
+    document.getElementById("root")
 )
