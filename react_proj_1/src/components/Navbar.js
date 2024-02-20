9a537d17b491916879aeff35693112effcbc5cd1
kibetamos <amos.rotich@strathmore.edu>
2024-02-20 14:43:57 +0300

    SetupReact- Added styling

--- react_proj_1/src/components/Navbar.js
+++ react_proj_1/src/components/Navbar.js
@@ -4,13 +4,13 @@ Challenge: Complete the Navbar to match the design
 Hint: use the Figma file for the most accurate peek at the design
 (colors, sizes, fonts, etc.)
 */
-
+// import './index.css';
 import React from "react"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" />
+            <img src=".../src/images/copy.png" className="nav-logo" />
             <h3>ReactFacts</h3>
             <h4>React Course - Project 1</h4>
         </nav>
