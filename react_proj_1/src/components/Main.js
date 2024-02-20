149b39238758cf6a177f8ae40487ee1049d73334
kibetamos <amos.rotich@strathmore.edu>
2024-02-20 17:28:30 +0300

    SetupReact- Added styling

--- react_proj_1/src/components/Main.js
+++ react_proj_1/src/components/Main.js
@@ -1,18 +1,16 @@
 import React from "react"
 
 export default function Main() {
-    return(
-        <div>
-                <h1>Fun Facts about React</h1>
-            <ol>
-                <li>Popularity </li>
-                <li>Declarative Syntax </li>
-                <li>Virtual DOM </li>
-                <li>Ecosystem</li>
-                <li>Backed by Facebook</li>
-                <li>Learning Opportunities</li>
-            </ol>
-        </div>
-
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
