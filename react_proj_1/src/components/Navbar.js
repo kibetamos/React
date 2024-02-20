9a734a6193f4c481abbde124c27ed08929c6b412
kibetamos <amos.rotich@strathmore.edu>
2024-02-20 12:41:31 +0300

    Setup React

--- react_proj_1/src/components/Navbar.js
+++ react_proj_1/src/components/Navbar.js
@@ -9,17 +9,11 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <header>
-        <nav className="nav">
-            {/* <img src="../../images/logo512.png" className="nav-logo" alt="Logo"/> */}
-            <img src="../images/logo512.png" />
-                <ul className= "nav-items">
-                    <li>React Course - Project one</li>
-                    {/* <li>About</li>
-                    <li>Contact</li> */}
-                </ul>
-            </nav>
-            </header>
+        <nav>
+            <img src="../images/react-icon-small.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
 
