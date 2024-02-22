1cd7f88416bcface03028624e174bdb2777a13fd
kibetamos <amos.rotich@strathmore.edu>
2024-02-22 16:17:27 +0300

    Adding Airbnb app- File structure

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -2,18 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import './index.css';
 import App from "./App"
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(<App />, document.getElementById("root"))
 
 
-// import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
