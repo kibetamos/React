08512a63f5c2df675d59cf7b6234e6b455405576
kibetamos <amos.rotich@strathmore.edu>
2024-02-22 18:35:21 +0300

    Airbnb styling -Add Hero component

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,8 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
+import './index.css';
+import Hero from "./components/Hero";
 /*
 Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+/*
+Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
 */
 
@@ -10,7 +15,7 @@ export default function App() {
     return (
         <div>
           <Navbar />
-          
+          <Hero />
         </div>
     )
 }
---
+++ airbnb/src/components/Hero.js
@@ -0,0 +1,10 @@
+import Recat from "react"
+
+
+export default function Hero(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
