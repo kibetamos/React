138347f18803c90a6390bd7de5424b1f0aa3ebdf
kibetamos <amos.rotich@strathmore.edu>
2024-02-22 10:12:52 +0300

    Adding Airbnb app- File structure

--- react_proj_1/src/components/Navbar.js
+++ react_proj_1/src/components/Navbar.js
@@ -5,12 +5,14 @@ Hint: use the Figma file for the most accurate peek at the design
 (colors, sizes, fonts, etc.)
 */
 
-
+// import logo from './logo.svg';
+// /home/amos/projects/React/react_proj_1/src/logo.svg
 import React from "react"
 
 export default function Navbar() {
     return (
         <nav>
+            
             <img src="./images/react-logo.png" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
