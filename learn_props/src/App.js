2b62460b32e90f3c318091241527a80a0193b36f
kibetamos <amos.rotich@strathmore.edu>
2024-02-24 14:48:43 +0300

    Learn props - create contact component

--- learn_props/src/App.js
+++ learn_props/src/App.js
@@ -12,69 +12,32 @@ import './style.css';
 function App() {
     return (
         <div className="contacts">
-            <Contact />
-            < Contact 
-            img src="./images/fluffykins.png"
-            name = "Fluffykins"
-            email = 'fluff@me.com'
-            phone = '(212) 555-2345'
+             <Contact 
+                img="./images/mr-whiskerson.png"
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
             />
-
-            < Contact 
-            img src="./images/felix.png"
-            name = "Felix"
-            email = 'thecat@hotmail.com'
-            phone = '(212) 555-4567'
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
             />
-
-            < Contact 
-            
-            img src="./images/pumpkin.png"
-            name = "Pumpkin"
-            email = 'pumpkin@scrimba.com'
-            phone = '(0800) CAT KING'
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
             />
         </div>
     )
 }
 
 export default App
-
-{/* <div className="contact-card">
-                <img src="./images/fluffykins.png"/>
-                <h3>Fluffykins</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(212) 555-2345</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>fluff@me.com</p>
-                </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src="./images/felix.png"/>
-                <h3>Felix</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>thecat@hotmail.com</p>
-                </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src="./images/pumpkin.png"/>
-                <h3>Pumpkin</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(0800) CAT KING</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>pumpkin@scrimba.com</p>
-                </div>
-            </div> */}
