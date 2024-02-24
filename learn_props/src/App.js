61887f907a55671ca374ce909f8522e34c52d436
kibetamos <amos.rotich@strathmore.edu>
2024-02-24 14:24:36 +0300

    Added learn props app

--- learn_props/src/App.js
+++ learn_props/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './style.css';
+/* Challenge:
+
+- Create a Contact.js component in another file
+- Move one of the contact card divs below into that file
+- import and render 4 instances of that contact card
+    - Think ahead: what's the problem with doing it this way?
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="contacts">
+        
+            <div className="contact-card">
+                <img src="./images/mr-whiskerson.png"/>
+                <h3>Mr. Whiskerson</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>(212) 555-1234</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>mr.whiskaz@catnap.meow</p>
+                </div>
+            </div>
+            
+            <div className="contact-card">
+                <img src="./images/fluffykins.png"/>
+                <h3>Fluffykins</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>(212) 555-2345</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>fluff@me.com</p>
+                </div>
+            </div>
+            
+            <div className="contact-card">
+                <img src="./images/felix.png"/>
+                <h3>Felix</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>(212) 555-4567</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>thecat@hotmail.com</p>
+                </div>
+            </div>
+            
+            <div className="contact-card">
+                <img src="./images/pumpkin.png"/>
+                <h3>Pumpkin</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>(0800) CAT KING</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>pumpkin@scrimba.com</p>
+                </div>
+            </div>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
