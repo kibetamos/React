e3c6d72d5948d045c61087ca78df66267d29b3f0
kibetamos <amos.rotich@strathmore.edu>
2024-02-24 15:17:04 +0300

    Learn props - create Destructive props

--- learn_props/src/components/Contact.js
+++ learn_props/src/components/Contact.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 
-export default function Contact(props){
+export default function Contact({img, name, phone, email}){
 
     // console.log(props)
     return(
         <div className="contact-card">
-        <img src={props.img}/>
-        <h3>{props.name}</h3>
+        <img src={img}/>
+        <h3>{name}</h3>
         <div className="info-group">
             <img src="./images/phone-icon.png" />
-            <p>{props.phone}</p>
+            <p>{phone}</p>
         </div>
         <div className="info-group">
             <img src="./images/mail-icon.png" />
-            <p>{props.email}</p>
+            <p>{email}</p>
         </div>
     </div>
     )
