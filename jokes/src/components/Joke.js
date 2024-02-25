1d934a0100d4a00f825e90f0363ab7d61db1e8cf
kibetamos <amos.rotich@strathmore.edu>
2024-02-25 10:41:42 +0300

    Create Jokes app - props

--- jokes/src/components/Joke.js
+++ jokes/src/components/Joke.js
@@ -5,7 +5,7 @@ export default function Joke(props){
     console.log(props)
     return(
         <div>
-        <h3>{props.setup}</h3>
+        {props.setup && <h3>Setup: {props.setup}</h3>}
         <p>{props.punchline}</p>
         <hr />
     </div>
