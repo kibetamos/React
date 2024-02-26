7fb63cb804bc6ae6b07370ee9109b489719b43f1
kibetamos <amos.rotich@strathmore.edu>
2024-02-26 11:34:57 +0300

    Jokes app - props

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Joke from "./components/Joke";
 import './style.css';
+import jokesData from "./jokesData";
 /*
 Challenge: Think critically - how would you pass in a prop that wasn't
 a string datatype.
@@ -14,53 +15,25 @@ of whether the joke is a pun (`isPun`).
 Challenge: turn the strings in the array into <h3> elements instead
 */
 
-function App() {
-    const colors = [           <h3>Red</h3>, 
-    <h3>Orange</h3>, 
-    <h3>Yellow</h3>,
-    <h3>Green</h3>,
-    <h3>Blue</h3>,
-    <h3>Indigo</h3>,
-    <h3>Violet</h3>]
+// const capitalized = names.map((name) => {
+//     return name[0].toUpperCase() + name.slice(1)
+// })
+
+
+/*
+Challenge: See if you can correctly pass the necessary props to the 
+Joke component in the .map() (and render the jokeElements array) so 
+the jokes show up on the page again
+*/
+
+export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
     return (
         <div>
-            {colors}
+           {jokeElements} 
         </div>
         
     )
 }
-
-export default App
-
-
-// <div className="jokes">
-        //     <Joke 
-        //         punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-        //     />
-
-        //      <Joke
-        //      setup = "I got my daughter a fridge for her birthday."
-        //      punchline="I can't wait to see her face light up when she opens it." 
-        //      />
-
-        //      <Joke 
-        //      setup = "How did the hacker escape the police?"
-        //      punchline="He just ransomware!"
-        //      />
-
-        //     <Joke 
-        //      setup = "Why dont pirates travel on mountain roads "
-        //      punchline = " Scurvy " 
-        //      />
-
-        //     <Joke 
-        //      setup = "Why do bees stay in the hive in the winter? "
-        //      punchline="Swarm." 
-        //      />
-
-        //     <Joke 
-        //      setup = "What's the best thing about Switzerland?"
-        //      punchline="I don't know, but the flag is a big plus!" 
-        //      />
-            
-        // </div>
