0b473c190c7c5da49a1c414f90931b3daca581e4
kibetamos <amos.rotich@strathmore.edu>
2024-02-26 12:29:13 +0300

    Airbnb application- map

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -25,6 +25,20 @@ Challenge: Pass props to the Card component and display that data
 */
 
 
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+
+
 export default function App() {
     return (
         <div>
