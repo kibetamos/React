2850107f9b715514eb82d7bf6ba757f840d4d2df
kibetamos <amos.rotich@strathmore.edu>
2024-03-01 11:04:14 +0300

    Added styling

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -3,6 +3,7 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
+           {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
             <div className="card--badge">SOLD OUT</div>
             <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
