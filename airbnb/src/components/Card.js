590d5fa4c10846994f48139e8dd8d1bba5ef51c8
kibetamos <amos.rotich@strathmore.edu>
2024-03-13 13:02:31 +0300

    Added component

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -14,18 +14,22 @@ export default function Card(props) {
     1. Display the correct text in the badge based on the logic above
     2. Only display the badge if badgeText has a value
     */
+    /*
+    Challenge: Fix our component! 😱
+    */
+
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`} className="card--image" />
+            <img src={`../images/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span className="gray">({props.item.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
             {/* <p className="">{props.price}</p> */}
         </div>
     )
