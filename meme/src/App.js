4af265a22c96ed9a32f82fa0b0aab9d81c010dc4
kibetamos <amos.rotich@strathmore.edu>
2024-03-17 14:33:39 +0300

    Add styling

--- meme/src/App.js
+++ meme/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 /**
  * Challenge: Build the Header component
  */
@@ -7,6 +8,7 @@ export default function App() {
     return (
     <div>
     <Header />
+    <Meme />
       </div>
     )
   }
---
+++ meme/src/components/Meme.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <div>
+                    <label>Top Text
+                        <input
+                            type="text"
+                            placeholder="Shut up"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Bottom Text
+                    <input
+                            type="text"
+                            placeholder="and take my money"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
