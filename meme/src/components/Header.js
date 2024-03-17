fbc8708dd24660c09a93cfade055e7a6a8eafe35
kibetamos <amos.rotich@strathmore.edu>
2024-03-17 14:22:49 +0300

    restructure

--- meme/src/components/Header.js
+++ meme/src/components/Header.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from "react"
 
-
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <header className="header">
-        <img src="./images/troll-face.png" />
-        <h2>Header component</h2>
-        <h4>React Course - Project 3</h4>
-    </header>
+            <img 
+                src="./images/trollge.jpg" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
