8ad2d821b5faef5e4b6555e00b8a80b2f699eb4d
kibetamos <amos.rotich@strathmore.edu>
2024-04-14 20:41:18 +0300

    added array destructuring

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -3,13 +3,27 @@ import React from "react"
 export default function App() {
   // const state = "Yes"
 //   lets do array destructuirng
-  const [result, funct] = React.useState("Yes")
-  console.log(result) 
+  const [isImportant, setIsImportant] = React.useState("Yes")
+//   setIsImportant("Noe")
+
+/**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+
+ function handleClick(){
+    
+ }
+
+//   console.log(result) 
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
             <div className="state--value">
-                <h1>{result}</h1>
+                <h1>{isImportant}</h1>
             </div>
         </div>
     )
