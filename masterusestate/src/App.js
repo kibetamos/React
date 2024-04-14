54450651b09f8672db660163094123c87b3d0946
kibetamos <amos.rotich@strathmore.edu>
2024-04-14 20:34:27 +0300

    added array destructuring

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 
 export default function App() {
   // const state = "Yes"
-  const result = React.useState("Yes")
+//   lets do array destructuirng
+  const [result, funct] = React.useState("Yes")
   console.log(result) 
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
             <div className="state--value">
-                <h1>{result[0]}</h1>
+                <h1>{result}</h1>
             </div>
         </div>
     )
