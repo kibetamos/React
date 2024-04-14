71f4029c17ce7d803819a4565674d2d9c5ce6d69
kibetamos <amos.rotich@strathmore.edu>
2024-04-14 18:30:51 +0300

    added master of usestate

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -1,12 +1,9 @@
 import React from "react"
 
 export default function App() {
-    /**
-     * Challenge: Replace our hard-coded "Yes" on the page with 
-     * some state initiated with React.useState()
-     */
-    const result = React.useState("Yes")
-    console.log(result)
+  // const state = "Yes"
+  const result = React.useState("Yes")
+  console.log(result) 
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
