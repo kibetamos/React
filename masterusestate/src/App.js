0e14454de596a50cd73348ba0c4c4ddb286c9739
kibetamos <amos.rotich@strathmore.edu>
2024-04-15 09:43:35 +0300

    added callback function

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -42,7 +42,20 @@ export default function App() {
     function add(){
 
         // console.log('add')
-        setCount(count + 1)
+        // setCount(count + 1)
+
+        /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. 
+     * This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+        setCount(prevCount => prevCount + 1)
 
     }
 
@@ -50,7 +63,7 @@ export default function App() {
 
         setCount(count - 1)
     }
-    
+
     return (
         <div className="counter">
             <button className="counter--minus" onClick={minus}>–</button>
