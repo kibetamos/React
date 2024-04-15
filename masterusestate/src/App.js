c672977ba73f56635ef628ae26384e5a456cb20e
kibetamos <amos.rotich@strathmore.edu>
2024-04-15 09:21:07 +0300

    added array destructuring

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -1,29 +1,45 @@
 import React from "react"
 
 export default function App() {
-  // const state = "Yes"
-//   lets do array destructuirng
-  const [isImportant, setIsImportant] = React.useState("Yes")
-//   setIsImportant("Noe")
+//   // const state = "Yes"
+// //   lets do array destructuirng
+//   const [isImportant, setIsImportant] = React.useState("Yes")
+// //   setIsImportant("Noe")
 
-/**
-     * Challenge: 
-     * 1. Create a function called `handleClick` that runs
-     *    setIsImportant("No")
-     * 2. add a click event listener to the div.state--value
-     *    that runs `handleClick` when the div is clicked.
+// /**
+//      * Challenge: 
+//      * 1. Create a function called `handleClick` that runs
+//      *    setIsImportant("No")
+//      * 2. add a click event listener to the div.state--value
+//      *    that runs `handleClick` when the div is clicked.
+//      */
+//  function handleClick(){
+//     setIsImportant("No") 
+//  }
+// //   console.log(result) 
+//     return (
+//         <div className="state">
+
+//             <h1 className="state--title">Is state important to know?</h1>
+//             <div className="state--value" onClick={handleClick}>
+//                 <h1>{isImportant}</h1>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
+    /**
+     * Challenge: Set up state to track our count (initial value is 0)
      */
- function handleClick(){
-    setIsImportant("No") 
- }
-//   console.log(result) 
     return (
-        <div className="state">
-
-            <h1 className="state--title">Is state important to know?</h1>
-            <div className="state--value" onClick={handleClick}>
-                <h1>{isImportant}</h1>
+        <div className="counter">
+            <button className="counter--minus">–</button>
+            <div className="counter--count">
+                <h1>0</h1>
             </div>
+            <button className="counter--plus">+</button>
         </div>
     )
 }
