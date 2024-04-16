decb8205d71362f5dbcf3406ef478a167d3a1587
kibetamos <amos.rotich@strathmore.edu>
2024-04-16 10:19:55 +0300

    ternary practice

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -90,6 +90,12 @@ export default function App() {
      * Challenge: Replace the if/else below with a ternary
      * to determine the text that should display on the page
      */
+
+    // Another example
+
+    const isBuyingLand = true
+    let vote = isBuyingLand ? 'Buying Land': 'Not Buying'; 
+
     const isGoingOut = false
     
     let answer  = isGoingOut ? 'Yes' : 'No';// Use ternary here
@@ -104,6 +110,7 @@ export default function App() {
             <h1 className="state--title">Do I feel like going out tonight?</h1>
             <div className="state--value">
                 <h1>{answer}</h1>
+                
             </div>
         </div>
     )
