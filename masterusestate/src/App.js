ac69c3fff2a88c171930129ff634215bbaa16693
kibetamos <amos.rotich@strathmore.edu>
2024-04-16 12:03:01 +0300

    ternary

--- masterusestate/src/App.js
+++ masterusestate/src/App.js
@@ -153,9 +153,9 @@ export default function App() {
     return (
         <div className="state">
             <h1 className="state--title">Do I feel like going out tonight?</h1>
-            <div className="state--value" onClick={changeMind}>
-                <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
-            </div>
+            <button className="state--value" onClick={changeMind}>
+                {isGoingOut ? 'Yes' : 'No'}
+            </button>
         </div>
     )
 }
