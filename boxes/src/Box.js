87a2aa71b7fc928eb97ec51242c88aea68e570f8
kibetamos <amos.rotich@strathmore.edu>
2024-04-23 11:24:55 +0300

    map elements

--- boxes/src/Box.js
+++ boxes/src/Box.js
@@ -2,6 +2,21 @@ import React from "react"
 import boxes from "./boxes"
 
 export default function Box(props) {
+
+        /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+
+
+
+        
     const styles = {
         backgroundColor: props.on ? "#222222" : "none"
     }
