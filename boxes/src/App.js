86fdb7166fb4be3b880011afee8d49824a90a659
kibetamos <amos.rotich@strathmore.edu>
2024-04-23 10:08:33 +0300

    map elements

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import boxes from "./boxes"
+import './style.css';
 
 export default function App() {
     /**
@@ -10,9 +11,17 @@ export default function App() {
      *    as an empty square (black border, transparent bg color)
      *    (Don't worry about using the "on" property yet)
      */
+    const [squares, setSquares] = React.useState(boxes)
+
+    const squareElements = squares.map(square => (
+
+      <div className="box" key={square.id}></div>
+    ))
+
+    // console.log(boxes)
     return (
-        <main>
-            <h1>Boxes will go here, </h1>
+       <main>
+            {squareElements}
         </main>
     )
 }
