a5fc62562548649a9b9764689e3a4703dd0dbe69
kibetamos <amos.rotich@strathmore.edu>
2024-04-27 20:01:04 +0300

    map elements

--- boxes/src/Box.js
+++ boxes/src/Box.js
@@ -21,6 +21,7 @@ export default function Box(props) {
     const styles = {
         backgroundColor: on ? "#222222" : "transparent"
     }
+    
     // return (
     //      <div style={styles} className="box" onClick={toggle}></div>
     // )
