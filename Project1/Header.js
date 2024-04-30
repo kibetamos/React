9d10817cf9fafd73c633a39f5328af1b8a287f1f
kibetamos <amos.rotich@strathmore.edu>
2024-04-30 08:57:12 +0300

    add readme

--- Project1/Header.js
+++ Project1/Header.js
@@ -6,7 +6,6 @@ export default function Header() {
                 <img src="./react-logo.png" className="nav-logo" />
                 <ul className="nav-items">
                     <li>Pricing</li>
-                    
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
