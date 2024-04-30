c97ac40119d02b6026e743ad38c938f4f17fedf3
kibetamos <amos.rotich@strathmore.edu>
2024-04-30 12:17:41 +0300

    conditional rendering

--- jokes/src/components/Joke.js
+++ jokes/src/components/Joke.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+
 
 export default function Joke(props){
+    const [shown, setisShow] = React.useState("false")
+
+    function toggle (){
+        console.log("Clicked")
+    }
+    
     console.log(props)
     return(
         <div>
             {props.setup && <h3>Setup: {props.setup}</h3>}
             <p>Punchline: {props.punchline}</p>
+            <button onClick={toggle}>Click</button>
             <hr />
         </div>
 
