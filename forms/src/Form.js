0faa6c575ce006b1956e819be85aaedff20233a1
kibetamos <amos.rotich@strathmore.edu>
2024-05-01 19:17:24 +0300

    Lets create React forms

---
+++ forms/src/Form.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Form() {
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+            />
+        </form>
+    )
+}
