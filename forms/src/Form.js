f2b2c92e3e9618aa43919918e6419f23fa6a002e
kibetamos <amos.rotich@strathmore.edu>
2024-05-02 23:46:14 +0300

    Lets create React forms

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -1,11 +1,18 @@
 import React from "react"
 
 export default function Form() {
+
+    const [firstName, setFirstName] = React.useState("")
+
+    function handlechange(){
+        console.log('Changed')
+    }
     return (
         <form>
             <input
                 type="text"
                 placeholder="First Name"
+                onChange={handlechange()}
             />
          
         </form> 
