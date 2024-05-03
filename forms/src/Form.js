4b8c017da19c444a43c2f6910f06fbae6a2331f0
kibetamos <amos.rotich@strathmore.edu>
2024-05-03 07:31:46 +0300

    Lets create React forms

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -4,7 +4,8 @@ export default function Form() {
 
     const [firstName, setFirstName] = React.useState("")
 
-    function handleChange(){
+    function handleChange(event){
+        console.log(event.target)
         console.log('Changed')
     }
     return (
