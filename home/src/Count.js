c88c98c5f9bd6428f4938ab5dc20ff6aba4e67e9
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-14 12:09:57 +0300

    learning useeffect

--- home/src/Count.js
+++ home/src/Count.js
@@ -2,26 +2,24 @@ import React from "react"
 
 export default function Count() {
     //Define state variables
-
     const [starWarsData, setStarWarsData] = React.useState({})
     const [count, setCount] = React.useState(0)
     
-    // Log that the component is rendered
-    console.log("Component rendered")
-    
-    // side effects
-    // Define side effects using React.useEffect()
+    /**
+     * Quiz:
+     * 1. What will happen if I put back our Star Wars API call
+     *    into the effect function?
+     * 2. How will the useEffect be different if I use 
+     *    setStarWarsData() instead of console.log()
+     * 
+     *  * 3. What SHOULD be in our dependencies array in this case?
+     */
     React.useEffect(function() {
-        // This effect runs when the component mounts or updates
-
-        console.log("Effect ran")
-        // Example side effect: fetching Star Wars data (currently commented out)
-        // fetch("https://swapi.dev/api/people/1")
-        //     .then(res => res.json())
-        //     .then(data => console.log(data))
-    }, [count])
+        fetch("https://swapi.dev/api/people/1")
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [])
     
-    // Return JSX for rendering
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
