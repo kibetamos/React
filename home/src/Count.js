154c592275303209d556640e3401b0249e95aca7
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-14 11:02:52 +0300

    learning useeffect

--- home/src/Count.js
+++ home/src/Count.js
@@ -1,20 +1,27 @@
 import React from "react"
 
 export default function Count() {
+    //Define state variables
+
     const [starWarsData, setStarWarsData] = React.useState({})
     const [count, setCount] = React.useState(0)
     
+    // Log that the component is rendered
     console.log("Component rendered")
     
     // side effects
+    // Define side effects using React.useEffect()
     React.useEffect(function() {
+        // This effect runs when the component mounts or updates
+
         console.log("Effect ran")
+        // Example side effect: fetching Star Wars data (currently commented out)
         // fetch("https://swapi.dev/api/people/1")
         //     .then(res => res.json())
         //     .then(data => console.log(data))
     })
     
-    
+    // Return JSX for rendering
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
