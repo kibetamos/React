37e6ca96ef489fc8a5269ab2d4ff380a4ea39929
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-15 16:59:59 +0300

    added challenge to react project 1

--- react_proj_1/src/components/Navbar.js
+++ react_proj_1/src/components/Navbar.js
@@ -9,11 +9,13 @@ Hint: use the Figma file for the most accurate peek at the design
 // /home/amos/projects/React/react_proj_1/src/logo.svg
 import React from "react"
 
+
 export default function Navbar() {
     return (
         <nav>
             
-            <img src="./images/react-logo.png" className="nav--icon" />
+            <img src="/images/react-logo.png" className="nav--icon" />
+     {/* /images/react-logo.png */}
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
