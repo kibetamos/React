abea803307fb4fa8ff417c5ade83dac3157261ae
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-16 16:33:46 +0300

    added note summary title

--- notes_app/src/components/Sidebar.js
+++ notes_app/src/components/Sidebar.js
@@ -17,7 +17,7 @@ export default function Sidebar(props) {
      */
 
 
-        
+
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
@@ -27,7 +27,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
