b813c81c45f12bc575ec334bbe12f570b7817f64
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-16 17:36:54 +0300

    added delete function

--- notes_app/src/App.js
+++ notes_app/src/App.js
@@ -112,6 +112,26 @@ export default function App() {
         // }))
     }
     
+
+       /**
+     * Challenge: complete and implement the deleteNote function
+     * 
+     * Hints: 
+     * 1. What array method can be used to return a new
+     *    array that has filtered out an item based 
+     *    on a condition?
+     * 2. Notice the parameters being based to the function
+     *    and think about how both of those parameters
+     *    can be passed in during the onClick event handler
+     */
+    
+       function deleteNote(event, noteId) {
+        event.stopPropagation()
+        // Your code here
+    }
+
+
+    
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
