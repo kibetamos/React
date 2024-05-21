6ba4864fdb5264d4930e358558ef11a97af94a07
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-21 13:52:57 +0300

    form

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -42,7 +42,6 @@ export default function Form() {
     //     setLastName(event.target.value)
 
     //}
-
     function handleSubmit(event){
         event.preventDefault()
         // submitToApi(formData)
