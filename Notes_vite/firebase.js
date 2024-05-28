49579841605b226ee52e14182961382b0be00da8
Kibet Amos <amos.rotich@strathmore.edu>
2024-05-28 10:54:51 +0300

    remove vite project

---
+++ Notes_vite/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAw0T6VhQejyDFQl9_xAZazMcjtyKcxZQ",
+  authDomain: "react-notes-fa23c.firebaseapp.com",
+  projectId: "react-notes-fa23c",
+  storageBucket: "react-notes-fa23c.appspot.com",
+  messagingSenderId: "1029433821061",
+  appId: "1:1029433821061:web:8f1eeaed156689ead866ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
