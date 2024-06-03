3030cd1bb99929bbb1a1717e8c65f607042209b0
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-03 17:04:20 +0300

    added greeting function

--- tinyreact/src/Greeting.jsx
+++ tinyreact/src/Greeting.jsx
@@ -3,7 +3,13 @@
 
 export default function Greeting(){
     return(
-        <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+        <h1>
+            
+            &quot;
+            I swear by my pretty floral bonnet, I will end you.
+            &quot;
+            
+            </h1>
         // <div>
         //     <p>Buuda</p>
         // </div>
