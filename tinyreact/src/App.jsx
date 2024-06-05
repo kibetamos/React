cf96086a99333dd7f69ac334c1bd2f9d33df2eac
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-05 08:29:51 +0300

    added updates

--- tinyreact/src/App.jsx
+++ tinyreact/src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
 
-import './App.css'
-
 export default function App() {
-  // const [count, setCount] = useState(0)
-  const animals = ['Lion', 'Cow','Snake','Lizard'];
 
+  const animals = ['Lion', 'Cow','Snake','Lizard'];
 
   return (
     <div>
-      <h1>Animals: </h1>
+     
       <ul>
-        {/* <li>Lion</li>
-        <li>Cow</li>
-        <li>Snake</li>
-        <li>Lizard</li> */}
-        {/* 
-      Dynamically rendering list items using the `map` function on the `animals` array.
-      Each item in the `animals` array is rendered as an <li> element with a unique `key` attribute.
-    */}
         {animals.map((animal) => {
           return <li key={animal}>                                                                           
           {animal}
@@ -30,3 +19,17 @@ export default function App() {
   )
 }
 
+
+// const names = ['Alice', 'Bob', 'Charlie', 'David'];
+
+// const NameList = () => {
+//   return (
+//     <ul>
+//       {names.map((name, index) => (
+//         <li key={index}>{name}</li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default NameList;
