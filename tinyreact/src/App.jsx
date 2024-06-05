40693a863365b8d9804271641336b57d5d77f385
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-05 11:08:58 +0300

    added learning jsx

--- tinyreact/src/App.jsx
+++ tinyreact/src/App.jsx
@@ -3,18 +3,22 @@ import { useState } from 'react'
 export default function App() {
 
   const animals = ['Lion', 'Cow','Snake','Lizard'];
+  const animalsList = animals.map((animal) => <li>{animal}</li>)
 
   return (
     <div>
       <h1>Animals: </h1>
       <ul>
-        {animals.map((animal) => (
+        {/* {animals.map((animal) => (
+          
           <li key={animal}>
 
             {animal}
             
             </li>
-        ))}
+        ))} */}
+        {animalsList}
+        
       </ul>
     </div>
   );
