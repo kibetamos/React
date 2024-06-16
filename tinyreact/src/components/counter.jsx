fa8cc716c5c985a5ab9a0cf713c2b1682fc86775
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-16 18:53:27 +0300

    counter

--- tinyreact/src/components/counter.jsx
+++ tinyreact/src/components/counter.jsx
@@ -6,6 +6,7 @@ export default function Counter() {
   return (
     <div>
       <p>Count: {count}</p>
+      
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
