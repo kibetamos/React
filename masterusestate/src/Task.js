f6724bd22220cecc89579f2a6cab0025ae5d4c8d
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-19 11:00:54 +0300

    useState

--- masterusestate/src/Task.js
+++ masterusestate/src/Task.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Task(){
     const[tasks, setTasks] = React.useState([
-    { id: 1, name: "Buy groceries", isCompleted: false },
-    { id: 2, name: "Walk the dog", isCompleted: true },
-    { id: 3, name: "Do laundry", isCompleted: false },
+
+        { id: 1, name: "Buy groceries", isCompleted: true },
+        { id: 2, name: "Walk the dog", isCompleted: true },
+        { id: 3, name: "Do laundry", isCompleted: true },
 ]);
 
 
@@ -13,9 +14,21 @@ return(
     <div>
     {tasks.map(task => (
         <p key={task.id}>
-            {task.name}
+           {task.id} 
+           
+           .{task.name} 
+
+           <input 
+            type="checkbox"
+            id="isComplete"
+            checked={task.isCompleted}
+            name="isCompleted"
+            />
         </p>
-    ))}
+
+    ))},
+    
+    
         </div>
 );
 
