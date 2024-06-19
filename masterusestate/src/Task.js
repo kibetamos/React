1bfec978b4b2cfe1fca87a25027f607da62d6e99
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-19 15:00:54 +0300

    remove duplicates

--- masterusestate/src/Task.js
+++ masterusestate/src/Task.js
@@ -1,88 +1,87 @@
 import React from "react";
 
-export default function Task(){
-    const [newTask, setNewTask] = React.useState("");
-    const[tasks, setTasks] = React.useState([
-        { id: 1, name: "Buy groceries", isCompleted: false },
-        { id: 2, name: "Walk the dog", isCompleted: false },
-        { id: 3, name: "Do laundry", isCompleted: false },
-]);
+// export default function Task(){
+//     const [newTask, setNewTask] = React.useState("");
+//     const[tasks, setTasks] = React.useState([
+//         { id: 1, name: "Buy groceries", isCompleted: false },
+//         { id: 2, name: "Walk the dog", isCompleted: false },
+//         { id: 3, name: "Do laundry", isCompleted: false },
+// ]);
 
-// const handleChange = (taskId) => {
-//     setTasks(tasks.map(task => {
-//         if (task.id === taskId) {
-//             return { ...task, isCompleted: !task.isCompleted };
-//         }
-//         return task;
+// // const handleChange = (taskId) => {
+// //     setTasks(tasks.map(task => {
+// //         if (task.id === taskId) {
+// //             return { ...task, isCompleted: !task.isCompleted };
+// //         }
+// //         return task;
 
         
-//     }));
+// //     }));
 
-    // console.log(tasks.isCompleted)
-    const handleCheckboxChange = (taskId) => {
-        setTasks(tasks.map(task => {
-            if (task.id === taskId) {
-                return { ...task, isCompleted: !task.isCompleted };
-            }
-            return task;
-        }));
-    };
+//     // console.log(tasks.isCompleted)
+//     const handleCheckboxChange = (taskId) => {
+//         setTasks(tasks.map(task => {
+//             if (task.id === taskId) {
+//                 return { ...task, isCompleted: !task.isCompleted };
+//             }
+//             return task;
+//         }));
+//     };
 
-const handleTaskChange = (e) => {
-        setNewTask(e.target.value);
-    };
+// const handleTaskChange = (e) => {
+//         setNewTask(e.target.value);
+//     };
 
-function handleSubmit(event){
-    event.preventDefault();
-    if (newTask.trim() !== "") {
-        setTasks(prevTasks => [
-            ...prevTasks,
-            { id: prevTasks.length + 1, name: newTask, isCompleted: true },
-        ]);
-        // setNewTask("");
-    }
+// function handleSubmit(event){
+//     event.preventDefault();
+//     if (newTask.trim() !== "") {
+//         setTasks(prevTasks => [
+//             ...prevTasks,
+//             { id: prevTasks.length + 1, name: newTask, isCompleted: true },
+//         ]);
+//         // setNewTask("");
+//     }
 
-}
+// }
 
 
-return(
-    <div>
-        <form onSubmit={handleSubmit}>
-        <input
-            type="text"
-            placeholder="Add Task"
-            onChange={handleTaskChange}
-            name="task"
-            value={newTask}
-            // value={formData.firstName}
-            />
+// return(
+//     <div>
+//         <form onSubmit={handleSubmit}>
+//         <input
+//             type="text"
+//             placeholder="Add Task"
+//             onChange={handleTaskChange}
+//             name="task"
+//             value={newTask}
+//             // value={formData.firstName}
+//             />
 
-            <button>Add Task</button>
-</form>
+//             <button>Add Task</button>
+// </form>
 
-        {tasks.map(task => (
-        <h2 key={task.id}>
-           {task.id} 
+//         {tasks.map(task => (
+//         <h2 key={task.id}>
+//            {task.id} 
            
-           .{task.name} 
+//            .{task.name} 
 
-           <input
-                        type="checkbox"
-                        onChange={() => handleCheckboxChange(task.id)}
-                        checked={task.isCompleted}
-                        name="isCompleted"
-                    />
-        </h2>
+//            <input
+//                         type="checkbox"
+//                         onChange={() => handleCheckboxChange(task.id)}
+//                         checked={task.isCompleted}
+//                         name="isCompleted"
+//                     />
+//         </h2>
 
-    ))},
+//     ))},
             
     
     
-        </div>
-);
+//         </div>
+// );
 
-}
-import React from "react";
+// }
 
 export default function Task(){
     const [newTask, setNewTask] = React.useState("");
