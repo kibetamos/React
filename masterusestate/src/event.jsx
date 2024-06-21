4187cb6b69023f8704331d77a408ebaac8e65a7f
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-21 10:20:43 +0300

    added events

---
+++ masterusestate/src/event.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const EventHandlingComponent = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    console.log('Form submitted with value:', inputValue);
+  };
+
+  return (
+
+            <div>
+
+            <button onClick={handleClick}>Click Me!</button>
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+
+                type="text"
+
+                value={inputValue}
+
+                onChange={handleChange}
+
+                placeholder="Type something..."
+
+                />
+
+                <button type="submit">Submit</button>
+
+            </form>
+
+            {formSubmitted && <p>Form was submitted with value: {inputValue}</p>}
+            </div>
+  );
+  
+};
+
+export default EventHandlingComponent;
