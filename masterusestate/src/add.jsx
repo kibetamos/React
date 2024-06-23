b7941ca2545e73321b838dddcece95c1b315e68d
Kibet Amos <amos.rotich@strathmore.edu>
2024-06-23 20:34:47 +0300

    added add function

--- masterusestate/src/add.jsx
+++ masterusestate/src/add.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 export default function Add(){
-
+    cont [values, setValues] = React.useState({
+        number1: '',
+        number2: ''
+    })
 
 
     return(
-        
+
       <Form>
+        <input 
+        type="text"
+        placeholder="Number 1"
+        onChange={handlechange}
+        value={}/>
 
       </Form>
     )
