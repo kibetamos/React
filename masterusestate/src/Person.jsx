fdfb9f256b350723280f66032209ec48f6058ff2
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-01 12:45:21 +0300

    added more on state

--- masterusestate/src/Person.jsx
+++ masterusestate/src/Person.jsx
@@ -3,11 +3,17 @@ import React from "react";
 
 
 export default function Person(){
+    const[Person, setPerson] = React.useState({
+        name:"",
+        age:""
+    });
+
+
 
     return(
         
         <div>
-
+<h1>{personalbar.age}</h1>
         </div>
     )
 }
