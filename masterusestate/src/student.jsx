e9828056f94afb6518eeda7a561f195b90c65489
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-01 10:54:09 +0300

    added

--- masterusestate/src/student.jsx
+++ masterusestate/src/student.jsx
@@ -39,6 +39,7 @@ export default function Student(){
                 
                 />
                 
+                
                 <input 
                 type="text"
                 placeholder="Lastname"
