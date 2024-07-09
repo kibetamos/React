859f57eccb1e04d994604a0ef4018299e3823bb0
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-09 17:13:35 +0300

    add useEffect

--- masterusestate/src/components/useState/clock.jsx
+++ masterusestate/src/components/useState/clock.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 export default function Clock() {
 
+  const[counter, setCounter] = React.useState(0);
+
+  React.  useEffect(() => {
+    setInterval(() => {
+      setCounter(count => count + 1)
+    }, 1000);
+  }, [])
 
   return (
-    <div>counter</div>
+    <div>
+      <p>{counter} seconds have passed.</p>
+    </div>
   )
 }
