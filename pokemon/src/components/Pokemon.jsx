1ce868456c6614712fa9f3376c1bba7e0f2d2868
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-15 20:02:54 +0300

    create pseudocode

--- pokemon/src/components/Pokemon.jsx
+++ pokemon/src/components/Pokemon.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import './pokemon.css';
 export default function Pokemon() {
 
 
   return (
-    <div>
-        <card>Pokemon</card>
-        
-        </div>
+<div className="container">
+      <div className="card">
+        Pokemon
+      </div>
+    </div>
   )
 }
