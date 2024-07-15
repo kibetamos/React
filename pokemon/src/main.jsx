1a26644fd470799ba9a0b0f27bac250c53c5c124
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-15 16:53:53 +0300

    start our pokemon project

--- pokemon/src/main.jsx
+++ pokemon/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
+import Pokemon from './components/Pokemon.jsx'
 // import App from 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Pokemon />
   </React.StrictMode>,
 )
