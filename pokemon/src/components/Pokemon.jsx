5d99b67cf59c5e6866a518a1f0649a5723a177ce
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-19 17:57:40 +0300

    added card

--- pokemon/src/components/Pokemon.jsx
+++ pokemon/src/components/Pokemon.jsx
@@ -4,10 +4,17 @@ export default function Pokemon() {
 
 
   return (
-<div className="container">
-      <div className="card">
-        Pokemon
+    
+<div className="cards-container">
+        {cards.map((card, index) => (
+          <div key={index} className="card" onClick={() => handleCardClick(card)}>
+            {card.type === 'image' ? (
+              <img src={card.url} alt={card.title} />
+            ) : (
+              <p>{card.text}</p>
+            )}
+          </div>
+        ))}
       </div>
-    </div>
   )
 }
