cc3837c774a17941aaf00299547f88df88f1a435
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-22 12:32:01 +0300

    add class input

--- masterusestate/src/components/classComponents/Task.jsx
+++ masterusestate/src/components/classComponents/Task.jsx
@@ -1,10 +1,41 @@
 import React   from "react";
 
-const functionalInput = ({ name }) =>{
+const Task = ({ name }) =>{
 
     const[todo, setTodos] = React.useState(["Just some demo tasks", "As an example"])
     const [inputVal, setInputVal] = React.useState("")
 
+const handleInputChange = (e) => {
+
+}
+    return (
+        <section>
+            <h3>{name}</h3>
+            <form>
+
+            <label htmlFor="task-entry">Enter a task: </label>
+                <input 
+                name="task"
+                type="text"
+                value={inputVal}
+                onChange={handleInputChange}
+
+                />
+
+                <button>Submot</button>
+            </form>
+        
+        <h4>All the Tasks</h4>
+
+        <ul>
+            {setTodos.map((todo) => (
+                <li key={todo}>{todo}</li>
+            ))}
+        </ul>
+        </section>
+    );
 
 
 }
+
+export default Task
