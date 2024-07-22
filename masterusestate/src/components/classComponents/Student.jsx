0f1ff70c37a04e32edd8e7fdc9230811752f0a26
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-22 15:00:45 +0300

    added class component

--- masterusestate/src/components/classComponents/Student.jsx
+++ masterusestate/src/components/classComponents/Student.jsx
@@ -3,18 +3,32 @@ import React from "react";
 export default function Student() {
     const [formData, setFormData] = React.useState("")
 
+    const handleInputChange = (e) => {
+        setFormData(e.target.value);
+    };   
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormData((formData) => [...formData]);
+        setFormData("")
+    };
+
   return (
 
     <div>
-<form>
+
+
+<form onClick={handleSubmit}>
 
 
         <input 
-
         placeholder="Name"
-        type=""
+        type="text"
         value={formData}
+        onChange={handleInputChange}
         />
+        <button>Submit</button>
 
 </form>
     </div>
