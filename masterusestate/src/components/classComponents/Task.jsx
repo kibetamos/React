322e13c9551a1b6aa75df8ca3b5eef726191d500
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-22 12:21:26 +0300

    add class input

---
+++ masterusestate/src/components/classComponents/Task.jsx
@@ -0,0 +1,10 @@
+import React   from "react";
+
+const functionalInput = ({ name }) =>{
+
+    const[todo, setTodos] = React.useState(["Just some demo tasks", "As an example"])
+    const [inputVal, setInputVal] = React.useState("")
+
+
+
+}
