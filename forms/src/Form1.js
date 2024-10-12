82ab0ab42b119b50f691ac3e5d8a49a1fc07eb81
kibetamos <amos.rotich@strathmore.edu>
2024-10-12 22:59:07 +0300

    update

--- forms/src/Form1.js
+++ forms/src/Form1.js
@@ -18,8 +18,9 @@ const [formData, setFormData] = React.useState({
 function handleChange(event) {
     const{name, value, type, checked} = event.target
     setFormData(prevFormData =>{
-    
+
         return{
+            
             ...prevFormData, [name]:type === "checkbox" ? checked :value
         }
     }
